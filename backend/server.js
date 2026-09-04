680af0c4d12a4fb1651e2adc8c4016682d3c3328
Kari Ganson <kariganson91@gmail.com>
2026-09-03 21:50:10 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const sequelize = require("./database/database");
 const User = require("./models/User");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
